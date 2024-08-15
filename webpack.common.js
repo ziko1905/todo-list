59d65e5e97e295997dd7b3cb54417947730f4955
Karlo Cehulic <karlocehlic@gmail.com>
2024-08-16 00:05:43 +0200

    Add all loaders to webpack confg

--- webpack.common.js
+++ webpack.common.js
@@ -8,11 +8,28 @@ module.exports = {
     plugins: [
         new HtmlWebpackPlugin({
             title: "Production",
+            template: "./src/template.html",
         }),
     ],
     output: {
         filename: "main.bundle.js",
         path: path.resolve(__dirname, "dist"),
         clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\html$/i,
+                loader: "html-loader",
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: "asset/resource",
+            },
+        ],
     },
 };
