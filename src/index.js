cf4e23b459f776b0d1409c6c809ebb596592dcae
Karlo Cehulic <karlocehlic@gmail.com>
2024-08-16 00:30:57 +0200

    Create task class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import "./styles.css"
+
+class Task {
+    constructor(title, description, date, priority, notes, check) {
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.priority = priority;
+        this.notes = notes;
+        this.check = check;
+    }
+}
