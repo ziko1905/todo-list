c11152a6a3aaa8bdbf7d4ba8d9d01010b523ca41
Karlo Cehulic <karlocehlic@gmail.com>
2024-08-16 01:56:29 +0200

    Add basic styling for cards

--- src/load.js
+++ src/load.js
@@ -1,7 +1,7 @@
 export function createNav() {
     const nav = document.querySelector("nav");
     let cards = [];
-    for (let n of ["Project"]) {
+    for (let n of ["Projects", "Today", "ByImportance", "ByDate"]) {
         const card = document.createElement("div")
         card.className = "card";
         card.textContent = n;
